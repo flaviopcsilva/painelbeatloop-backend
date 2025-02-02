b2a6f14fe4a6f54a828aed3016b978beffc68123
Flávio Peres <flaviopc2@gmail.com>
2025-02-02 17:16:47 -0300

    adicionando o cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Permitir qualquer domínio
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true, // Remove campos não esperados do body
     forbidNonWhitelisted: true, // Retorna erro se enviar um campo inválido
